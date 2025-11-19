5eab7fe83d559c8e345c2fb0cd283bb3fc7de94a
Isabelle <ilstone17@gmail.com>
2025-11-19 14:05:30 -0500

    contact page & hamburger menu

--- react-version/src/App.jsx
+++ react-version/src/App.jsx
@@ -6,6 +6,8 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 import Menu from './pages/Menu';
 import About from './pages/About';
+import Contact from './pages/Contact';
+
 
 
 function App() {
@@ -17,6 +19,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/menu" element={<Menu />} />
+          <Route path="/contact" element={<Contact />} />
 
         </Routes>
         <Footer />
