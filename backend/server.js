134c16c869c34eeb9bd1d8bd6baad56a05b53013
Isabelle <ilstone17@gmail.com>
2025-12-16 15:20:53 -0500

    bug fix

--- backend/server.js
+++ backend/server.js
@@ -15,21 +15,7 @@ mongoose.connect(uri)
     .then(() => console.log('hiya mongodb'))
     .catch(err => console.error('NOOOOOooooooo ', err));
 
-    app.use(cors({
-        origin: function(origin, callback) {
-            const allowedOrigins = [
-                'http://localhost:5173',
-                'https://bellas-bites.vercel.app'
-            ];
-            // allow any Vercel preview domain...
-            if (!origin || allowedOrigins.includes(origin) || origin.includes('.vercel.app')) {
-                callback(null, true);
-            } else {
-                callback(new Error('Not allowed by CORS'));
-            }
-        },
-        credentials: true
-    }));
+app.use(cors());
 
 app.use(express.json());
 
